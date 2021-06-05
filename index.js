e77790e458d02a2717e5323ac16cdda735884b82
Henry Ly <henryly213@gmail.com>
2021-06-05 00:00:18 -0500

    got both pages completed

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require("path")
+const app = express();
+const server = app.listen(3000, () => console.log("listening on port 3000"))
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, "public")))
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/home.html');
+});
+
+app.get("/chat", (req, res) => {
+  res.sendFile(__dirname + '/chat.html');
+})
+
+io.on('connection', (socket) => {
+  socket.on('send message', (msg, room) => {
+    if (room === "") {
+      socket.broadcast.emit("chat message", msg);
+    } else {
+      console.log("hit", room)
+      socket.to(room).emit("chat message", msg);
+    }
+  });
+
+  socket.on("join room", room => {
+    socket.join(room)
+  })
+});
+
